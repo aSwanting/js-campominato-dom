69139a8f67c66d12aa44b33ee6262fea081eddb7
aSwanting <gab.damico@gmail.com>
2023-10-16 16:12:02 +0200

    add: bomb tiles generate on grid start

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 
-function generateGrid(gridSize) {
 
+
+
+// Start game on PLAY
+document.getElementById("play-button").addEventListener("click", function () {
+
+    const difficulty = parseInt(document.getElementById("level-select").value)
+    let gridSize
+    !difficulty ? gridSize = 100 : difficulty === 1 ? gridSize = 81 : gridSize = 49
+
+    generateGrid(gridSize)
+    cliccableTiles()
+    addBombs(gridSize)
+
+})
+
+
+// Generate grid based on difficulty chosen
+function generateGrid(gridSize) {
+    
     const gridWrapper = document.getElementById("grid-wrapper")
     gridWrapper.className = "grid-wrapper grid-" + gridSize
     gridWrapper.innerHTML = ""
-
+    
     for (i = 0; i < gridSize; i++) {
         const box = document.createElement("div")
         gridWrapper.append(box)
@@ -13,30 +31,54 @@ function generateGrid(gridSize) {
     }
 }
 
+
+// Make tiles cliccable
 function cliccableTiles() {
-
+    
     const tiles = document.querySelectorAll(".tile")
-
+    
     tiles.forEach(function (tile, i) {
         tile.addEventListener("click", function () {
             tile.classList.toggle("selected")
             console.log(i + 1)
         })
     })
-
+    
 }
 
-document.getElementById("play-button").addEventListener("click", function () {
 
-    const difficulty = parseInt(document.getElementById("level-select").value)
+// Add bomb tiles to grid
+function addBombs(gridSize) {
+    
+    const tiles = document.querySelectorAll(".tile")
+    const bombTiles = randArray(1, gridSize, 16)
+    console.log(bombTiles)
+    
+    bombTiles.forEach(function (element, index) {
+        tiles[element - 1].classList.add("bomb-tile")
+    })
+}
 
-    let gridSize
 
-    !difficulty ? gridSize = 100 : difficulty === 1 ? gridSize = 81 : gridSize = 49
+// Generate random array
+function randArray(rangeMin, rangeMax, length) {
+    const randArrayNums = []
+    
+    while (randArrayNums.length < length) {
+        n = rand(rangeMin, rangeMax)
+        
+        if (!randArrayNums.includes(n)) {
+            randArrayNums.push(n)
+        }        
+    }    
+    
+    return randArrayNums    
+}
 
 
-    generateGrid(gridSize)
-    cliccableTiles()
-
-})
-
+// Random number function
+function rand(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
